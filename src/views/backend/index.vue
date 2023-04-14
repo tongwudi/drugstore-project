7c89de9eed656e6fc6eb67c93381c43bb980581c
litong <litong@archi-motive.com>
2023-04-14 16:32:20 +0800

    menu封装

--- src/views/backend/index.vue
+++ src/views/backend/index.vue
@@ -1,6 +1,10 @@
 <template>
   <el-container>
     <el-header>
+      <el-radio-group v-model="isCollapse">
+        <el-radio-button :label="false">展开</el-radio-button>
+        <el-radio-button :label="true">收起</el-radio-button>
+      </el-radio-group>
       <el-dropdown>
         <i class="el-icon-setting" style="margin-right: 15px"></i>
         <el-dropdown-menu slot="dropdown">
@@ -12,58 +16,34 @@
       <span>王小虎</span>
     </el-header>
     <el-container>
-      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-        <el-menu :default-openeds="['1', '3']">
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-message"></i>导航一
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+      <el-aside width="auto">
+        <el-scrollbar wrap-class="scrollbar-wrapper">
+          <el-menu
+            mode="vertical"
+            background-color="#304156"
+            text-color="#bfcbd9"
+            active-text-color="#409EFF"
+            :collapse="isCollapse"
+          >
+            <el-menu-item index="1">首页</el-menu-item>
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span slot="title">药品</span>
+              </template>
+              <el-menu-item index="2-1">药品管理</el-menu-item>
+              <el-menu-item index="2-2">库存管理</el-menu-item>
             </el-submenu>
-          </el-submenu>
-          <el-submenu index="2">
-            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="2-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+            <el-submenu index="3">
+              <template slot="title">
+                <i class="el-icon-setting"></i>
+                <span slot="title">系统</span>
+              </template>
+              <el-menu-item index="3-1">用户管理</el-menu-item>
+              <el-menu-item index="3-2">权限管理</el-menu-item>
             </el-submenu>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title"
-              ><i class="el-icon-setting"></i>导航三</template
-            >
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="3-1">选项1</el-menu-item>
-              <el-menu-item index="3-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="3-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="3-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-        </el-menu>
+          </el-menu>
+        </el-scrollbar>
       </el-aside>
 
       <el-main>
@@ -88,7 +68,8 @@ export default {
       address: '上海市普陀区金沙江路 1518 弄'
     }
     return {
-      tableData: Array(20).fill(item)
+      tableData: Array(10).fill(item),
+      isCollapse: false
     }
   }
 }
@@ -99,5 +80,29 @@ export default {
   line-height: 60px;
   background-color: #b3c0d1;
   color: #333;
+}
+
+.el-aside {
+  background-color: #304156;
+}
+.el-scrollbar {
+  height: 100%;
+  // 若没有 aside 包裹，高度会自动撑满
+  // background-color: #304156;
+}
+.scrollbar-wrapper {
+  overflow-x: hidden !important;
+}
+.el-menu {
+  border-right: none;
+}
+.el-menu:not(.el-menu--collapse) {
+  width: 200px;
+}
+.el-submenu .el-menu-item {
+  background-color: #1f2d3d !important;
+  &:hover {
+    background-color: #001528 !important;
+  }
 }
 </style>
