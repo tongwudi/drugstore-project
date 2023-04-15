cc7c04585f8132635b5b3a65d3f5b09516c22122
tongwudi <tongwudi@yeah.net>
2023-04-15 17:46:26 +0800

    后台页面

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,15 +1,13 @@
 <template>
   <el-container style="height: 100%" direction="vertical">
     <Navbar />
-    <el-container>
+    <el-container style="overflow: hidden">
       <Sidebar />
-      <!-- <el-main> -->
       <el-card>
         <transition name="fade-transform" mode="out-in">
           <router-view />
         </transition>
       </el-card>
-      <!-- </el-main> -->
     </el-container>
   </el-container>
 </template>
@@ -27,8 +25,22 @@ export default {
 
 <style lang="less" scoped>
 .el-card {
-  // height: 100%;
   flex: 1;
+  overflow: auto;
   margin: 10px;
+}
+
+::-webkit-scrollbar-track-piece {
+  background-color: #f0f2f5;
+}
+
+::-webkit-scrollbar {
+  width: 4px;
+  height: 4px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #d3dce6;
+  border-radius: 10px;
 }
 </style>
--- src/views/backend/drug.vue
+++ src/views/backend/drug.vue
@@ -1,15 +1,69 @@
 <template>
-  <div>
-    药品管理
+  <div class="app-container">
+    <el-form size="small" :inline="true" :model="formInline">
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select v-model="formInline.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="search">查询</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table size="small" :data="tableData">
+      <el-table-column prop="date" label="日期" width="140"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
+
+    <el-pagination
+      background
+      layout="total, prev, pager, next, jumper"
+      :hide-on-single-page="total === 0"
+      :page-size="10"
+      :current-page="currentPage"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
 
 <script>
-  export default {
-    
+export default {
+  data() {
+    const item = {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }
+    return {
+      formInline: {},
+      tableData: Array(10).fill(item),
+      currentPage: 1,
+      total: 10
+    }
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val
+      console.log(`当前页: ${val}`)
+    },
+    search() {}
   }
+}
 </script>
 
 <style lang="less" scoped>
-
+.el-table {
+  margin-bottom: 15px;
+}
 </style>
